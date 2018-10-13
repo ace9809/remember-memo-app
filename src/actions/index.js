e26ce2eda62292dc0cbb338d25416bbff12610bf
ace9809 <tktmdwns@naver.com>
2018-10-14 00:52:36 +0900

    Feature: Label 액션 생성

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,123 @@
 /**
  * Created by Ace on 2018. 10. 13..
  */
+import axios from 'axios';
+
+export const addLabel = title => dispatch => {
+  dispatch(addLabelStarted());
+  return axios.post(`http://114.207.113.7:18888/labels`, {
+    title : title
+  }).then(res => {
+    dispatch(addLabelSuccess(res.data));
+  }).catch(error => {
+    dispatch(apiFailure(error));
+    throw error;
+  })
+};
+
+export const getLabels = () => dispatch => {
+  dispatch(getLabelsStarted());
+  return axios.get(`http://114.207.113.7:18888/labels`)
+    .then(res => {
+      dispatch(getLabelsSuccess(res.data));
+    }).catch(error => {
+      dispatch(apiFailure(error));
+      throw error;
+    })
+};
+
+export const getLabel = (id) => dispatch => {
+  dispatch(getLabelStarted());
+  return axios.get(`http://114.207.113.7:18888/${id}`)
+    .then(res => {
+      dispatch(getLabelSuccess(res.data));
+    }).catch(error => {
+      dispatch(apiFailure(error));
+      throw error;
+    })
+};
+
+export const updateLabel = (id) => dispatch => {
+  dispatch(updateLabelStarted());
+  return axios.put(`http://114.207.113.7:18888/${id}`)
+    .then(res => {
+      dispatch(updateLabelSuccess(res.data));
+    }).catch(error => {
+      dispatch(apiFailure(error));
+      throw error;
+    })
+};
+
+export const deleteLabel = (id) => dispatch => {
+  dispatch(deleteLabelStarted());
+  return axios.delete(`http://114.207.113.7:18888/${id}`)
+    .then(res => {
+      dispatch(deleteLabelSuccess(res.data));
+    }).catch(error => {
+      dispatch(apiFailure(error));
+      throw error;
+    })
+};
+
+
+const apiFailure = error => ({
+  type: 'API_FAILURE',
+  payload: {
+    error
+  }
+});
+
+const addLabelStarted = () => ({
+  type: 'ADD_LABEL_STARTED',
+});
+
+const addLabelSuccess = data => ({
+  type: 'ADD_LABEL_SUCCESS',
+  payload: {
+    ...data
+  }
+});
+
+const getLabelsStarted = () => ({
+  type: 'GET_LABELS_STARTED',
+});
+
+const getLabelsSuccess = data => ({
+  type: 'GET_LABELS_SUCCESS',
+  payload: {
+    ...data
+  }
+});
+
+const getLabelStarted = () => ({
+  type: 'GET_LABEL_STARTED',
+});
+
+const getLabelSuccess = data => ({
+  type: 'GET_LABEL_SUCCESS',
+  payload: {
+    ...data
+  }
+});
+
+const updateLabelStarted = () => ({
+  type: 'UPDATE_LABEL_STARTED',
+});
+
+const updateLabelSuccess = data => ({
+  type: 'UPDATE_LABEL_SUCCESS',
+  payload: {
+    ...data
+  }
+});
+
+const deleteLabelStarted = () => ({
+  type: 'DELETE_LABEL_STARTED',
+});
+
+const deleteLabelSuccess = data => ({
+  type: 'DELETE_LABEL_SUCCESS',
+  payload: {
+    ...data
+  }
+});
