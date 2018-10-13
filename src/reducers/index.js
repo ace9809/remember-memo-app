dd5cceede7c88ca0bd77ac87259064041f409868
ace9809 <tktmdwns@naver.com>
2018-10-14 01:34:38 +0900

    Feature: 라벨 가져오는 리듀서 생성

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,28 @@
 /**
  * Created by Ace on 2018. 10. 13..
  */
+
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+  labels
+});
+
+function labels(state = {labels: []}, action) {
+  console.log('action.type', action);
+  switch(action.type) {
+    case 'GET_LABELS_STARTED':
+      return {
+        ...state
+      };
+    case 'GET_LABELS_SUCCESS':
+      return {
+        ...state,
+        labels: action.payload
+      };
+
+  }
+  return state;
+}
+
+export default rootReducer;
