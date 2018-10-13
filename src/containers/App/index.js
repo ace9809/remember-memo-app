16ef38a7f2b951f67547eccb697919bf4865b85b
ace9809 <tktmdwns@naver.com>
2018-10-13 13:41:36 +0900

    Initial commit

---
+++ src/containers/App/index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Ace on 2018. 10. 13..
+ */
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        index.js
+      </div>
+    );
+  }
+}
+
+export default App;
