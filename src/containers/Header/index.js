f7ab5859a11d32700c44047c1d9c0542e7ad9d4d
ace9809 <tktmdwns@naver.com>
2018-10-14 20:19:37 +0900

    Feature: 라벨 추가 메모 추가 아이콘 구현

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -3,6 +3,7 @@
  */
 import React from 'react';
 import styled from 'styled-components';
+import { FaEdit, FaTags } from 'react-icons/fa'
 import HeaderLogo from '../../assets/headerLogo.png';
 
 const HeaderWrapper = styled.div`
@@ -20,12 +21,46 @@ const LogoWrapper = styled.div`
   align-items: center;
 `;
 
+const NavWrapper = styled.div`
+  margin-right: 40px;
+  display: flex;
+`;
+
+const IconWrapper = styled.div`
+  width: 100%;
+  height: 40px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+`;
+
+const ColorFaEdit = styled(FaEdit)`
+  color: black;
+  width: 30px;
+  height: 50px;
+  margin-right: 30px;
+`;
+
+const ColorTags = styled(FaTags)`
+  color: black;
+  width: 30px;
+  height: 40px;
+`;
+
 const Header = () => {
   return (
     <HeaderWrapper>
       <LogoWrapper>
         <img src={HeaderLogo} height='50px' />
       </LogoWrapper>
+      <NavWrapper>
+        <IconWrapper>
+          <ColorFaEdit />
+        </IconWrapper>
+        <IconWrapper>
+          <ColorTags />
+        </IconWrapper>
+      </NavWrapper>
     </HeaderWrapper>
   )
 }
