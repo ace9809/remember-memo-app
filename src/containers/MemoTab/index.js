62131f00a8b78190bf6478c4ec5017194ef97143
ace9809 <tktmdwns@naver.com>
2018-10-16 01:42:27 +0900

    Feature: 아이콘 추가 및 뷰 수정

--- src/containers/MemoTab/index.js
+++ src/containers/MemoTab/index.js
@@ -7,6 +7,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
+import { MdMenu, MdFilterList } from 'react-icons/md'
 import MemoList from '../../components/MemoList';
 import { getMemos, getLabel} from '../../actions';
 
@@ -30,10 +31,27 @@ const TitleWrapper = styled.div`
   font-size: 20px;
 `;
 
-const CountWrapper = styled.div`
+const FooterWrapper = styled.div`
   margin-top: 80px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const CountWrapper = styled.div`
+  display: flex;
+  align-items: center;
   color: #a6a6a6;
   font-size: 15px;
+`;
+
+const IconNavWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const IconWrapper = styled.div`
+  margin-left: 20px;
+  font-size: 25px;
 `;
 
 class MemoTab extends Component {
@@ -61,11 +79,21 @@ class MemoTab extends Component {
               this.props.match.params.id === 'all' ? <div>전체</div> : <div>{title}</div>
             }
           </TitleWrapper>
-          <CountWrapper>
-            {
-              this.props.match.params.id === 'all' ? this.props.memos && <div>{this.props.memos.length}개의 노트</div> : memos && <div>{memos.length}개의 노트</div>
-            }
-          </CountWrapper>
+          <FooterWrapper>
+            <CountWrapper>
+              {
+                this.props.match.params.id === 'all' ? this.props.memos && <div>{this.props.memos.length}개의 노트</div> : memos && <div>{memos.length}개의 노트</div>
+              }
+            </CountWrapper>
+            <IconNavWrapper>
+              <IconWrapper>
+                <MdFilterList />
+              </IconWrapper>
+              <IconWrapper>
+                <MdMenu />
+              </IconWrapper>
+            </IconNavWrapper>
+          </FooterWrapper>
         </LabelInfoWrapper>
         <MemoListWrapper>
           {
