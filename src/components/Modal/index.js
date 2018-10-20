783c9d74f20966d96c48b2997122be860ac9774f
ace9809 <tktmdwns@naver.com>
2018-10-20 18:02:16 +0900

    Fix: 모달 컴포넌트 컨벤션 수정 및 불필요한 코드 제거

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -3,64 +3,16 @@
  */
 import React, { Component } from 'react';
 import ReactModal from 'react-modal';
-import styled from 'styled-components';
-
-const ModalWrapper = styled.div`
-  width: 100%;
-`;
-
-const ModalTitleWrapper = styled.div`
-  font-size: 20px;
-  font-weight: bold;
-`;
-
-const ModalContentWrapper = styled.div`
-  margin-top: 15px;
-  color: #777777;
-`;
-
-const InputWrapper = styled.div`
-  margin-top: 15px;
-`;
-
-const Input = styled.input`
-  width: 100%;
-  height: 38px;
-  box-sizing: border-box;
-  border: 2px solid black;
-  border-radius: 4px;
-  padding: 0 8px;
-  outline: 0;
-`;
-
-const ButtonWrapper = styled.div`
-  margin-top: 20px;
-  padding-top: 10px;
-  display: flex;
-  justify-content: flex-end;
-`;
-
-const Button = styled.button`
-  border-radius: 6px;
-  width: 80px;
-  height: 40px;
-  cursor: pointer;
-  margin-left: 20px;
-  background-color: ${(props) => props.backgroundColor};
-  color: ${(props) => props.color};
-  outline: 0;
-  border: ${(props) => props.border};
-`;
 
 const customStyles = {
-  content : {
-    width                 : '390px',
-    top                   : '50%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
+  content: {
+    width: '390px',
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom : 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
   }
 };
 
@@ -72,13 +24,17 @@ class Modal extends Component {
   };
 
   render() {
+    const {
+      open,
+      children
+    } = this.props;
     return (
       <ReactModal
-        isOpen={this.props.open}
+        isOpen={open}
         onRequestClose={this.closeModal}
         style={customStyles}
       >
-        {this.props.children}
+        {children}
       </ReactModal>
     )
   }
